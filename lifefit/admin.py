31336a8beac427031f1d7ecd5daa53f4d03d8908
FulvioTakara <fulviojp@gmail.com>
2022-04-08 02:12:43 -0300

    Models Category, Publication, Comment configurados e registrados no admin

--- lifefit/admin.py
+++ lifefit/admin.py
@@ -1,3 +1,43 @@
+from atexit import register
 from django.contrib import admin
 
-# Register your models here.
+from models import Category, Publication, Comment
+
+
+@register.admin(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'created',
+        'update',
+        'active',
+        'name_cat',
+    )
+
+
+@register.admin(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = (
+        'created',
+        'update',
+        'active',
+        'title',
+        'author',
+        'contents',
+        'excerpt',
+        'category',
+        'image',
+    )
+
+
+@register.admin(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'created',
+        'update',
+        'active',
+        'name',
+        'email',
+        'contents',
+        'publication',
+        'author',
+    )
--- lifefit/models.py
+++ lifefit/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Base(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(Base):
+    name_cat = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+
+    def __str__(self):
+        return self.name_cat
+
+
+class Publication(Base):
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    contents = models.TextField()
+    excerpt = models.TextField()
+    category = models.ForeignKey(
+        Category, on_delete=models.DO_NOTHING, blank=True, null=True)
+    image = models.ImageField(
+        upload_to='publication_img/%Y/%m/%d', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Publication'
+        verbose_name_plural = 'Publications'
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(Base):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    coments = models.TextField()
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return self.name
